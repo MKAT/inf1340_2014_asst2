7580ecc5e8af703462a9548c03663a967da24856
ohjchristine <christine.jihae.oh@gmail.com>
2014-11-05 23:18:37 -0500

    Notes for overall framework for logical process in order of validation flow

--- papers.py
+++ papers.py
@@ -20,15 +20,16 @@ with open("example_entries.json", "r") as file_reader:
     json_contents = json.loads(file_contents_example_entries)
     print(file_contents_example_entries)
 
+with open("watchlist.json", "r") as file_reader:
+    file_contents_watchlist = file_reader.read()
+    json_contents = json.loads (file_contents_watchlist)
+    print(file_contents_watchlist)
+
 with open("countries.json", "r") as file_reader:
     file_contents_countries = file_reader.read()
     json_contents = json.loads (file_contents_countries)
     print(file_contents_countries)
 
-with open("watchlist.json", "r") as file_reader:
-    file_contents_watchlist = file_reader.read()
-    json_contents = json.loads (file_contents_watchlist)
-    print(file_contents_watchlist)
 
 def decide(input_file, watchlist_file, countries_file):
     """
@@ -40,7 +41,40 @@ def decide(input_file, watchlist_file, countries_file):
         an entry or transit visa is required, and whether there is currently a medical advisory
     :return: List of strings. Possible values of strings are: "Accept", "Reject", "Secondary", and "Quarantine"
     """
-    return ["Reject"]
+
+    #For each traveler in the file, loop through each traveler and validate application information
+    for traveler in json_contents:
+
+        #Validate passport number
+        is_passport_valid = valid_passport_format(traveler['passport'])
+
+        #If passport number if valid, check watchlist for countries with medical advisory
+        does_country_have_medical_advisory = check_countries(travler['from']['via'])
+
+        #If country not on medical advisory list, check if all required information has been provided
+        is_information_complete = check_required_information(traveler)
+
+        # If all required information has been provided, check for valid birthday format
+        if is_passport_valid==True:
+            is_birthday_vaid = valid_date_format(traveler['birth_date'])
+
+        #If birthday format is valid, check to see if traveler's home country is KAN
+            #If home country is KAN and reason for entry is re-entry, accept
+
+        #If home country is not KAN, check to see reason for entry
+            #If reason for entry is visit and the traveler's country requires a visa, visitor visa required
+            #If reason for entry is transit and the traveler's country requires a visa, transit visa required
+
+        #If visa is required, check that visa date format is valid and did not expire
+
+        #If visa is valid, check if traveler's name is on watchlist
+
+            #If name is on watchlist secondary
+
+        #If traveler's name is not on watchlist, accept
+
+    else:
+        return ["Reject"]
 
 
 def valid_passport_format(passport_number):
@@ -58,6 +92,9 @@ def valid_passport_format(passport_number):
         return False
 
 
+
+
+
 def valid_date_format(date_string):
     """
     Checks whether a date has the format YYYY-mm-dd in numbers
@@ -69,3 +106,13 @@ def valid_date_format(date_string):
         return True
     except ValueError:
         return False
+
+def required_information_complete():
+    """
+    Checks whether all the required information has been provided
+    :param Personal and traveling information provided in strings
+           First Name
+           Last Name
+
+    :return:
+    """
